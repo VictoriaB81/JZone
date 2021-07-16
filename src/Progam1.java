df7ca7bf6b08d6cdb482c8b46c8f7d4ba48ea3d2
victoria <victoriabaretto81@gmail.com>
2021-07-16 19:36:39 +0530

    modified by pinky*

--- src/Progam1.java
+++ src/Progam1.java
@@ -4,6 +4,7 @@ public class Progam1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Pinky");
+		System.out.println("Heelo pinky1");
 	}
 
 }
